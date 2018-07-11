347ed2e77a26dbb842829ba6dd9aed56486a69f2
Alex <Alex@alexdembp.attlocal.net>
2018-07-11 08:14:40 -0500

    add images

--- src/com/bcgkyy/images/ImageMainView.java
+++ src/com/bcgkyy/images/ImageMainView.java
@@ -7,6 +7,7 @@ import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -17,9 +18,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import com.bcgkyy.db.DBManagement;
 import com.bcgkyy.demo.NumberImage;
+import javax.swing.JTextField;
 
 public class ImageMainView {
 
@@ -32,11 +35,16 @@ public class ImageMainView {
 	private final int IMAGE_HEIGHT = 384;
 	private final int IMAGE_LEFT = 0;
 	private final int IMAGE_RIGHT = 1;
-	private int firstImageIndex = 0;
-	private int secondImageIndex = 0;	
+	private int leftImageIndex = 0;
+	private int rightImageIndex = 0;	
 	
 	private JLabel label_image1;
 	private JLabel label_image2;
+	private JLabel label_leftNumber;
+	private JLabel label_rightNumber;
+	private JTextField textField_leftSearch;
+	private JTextField textField_rightSearch;
+	private JButton btnRightSearch;
 
 	/**
 	 * Launch the application.
@@ -86,65 +94,138 @@ public class ImageMainView {
 		JButton btnPre_image1 = new JButton("pre");
 		btnPre_image1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if(firstImageIndex == 0) {
-					firstImageIndex += numberImages.size();
+				if(leftImageIndex == 0) {
+					leftImageIndex += numberImages.size();
 				}
-				String firstImageUrl = numberImages.get((--firstImageIndex) % numberImages.size()).getUrls().get(IMAGE_LEFT);
-				label_image1.setIcon(getScaledImage(firstImageUrl, IMAGE_WIDTH, IMAGE_HEIGHT));
+				NumberImage numberImage = numberImages.get((--leftImageIndex) % numberImages.size());														
+				setImageAndNumber(numberImage.getUrls(), numberImage.getNumber(), leftImageIndex, true);
 				
 			}
 		});
-		btnPre_image1.setBounds(122, 473, 117, 29);
+		btnPre_image1.setBounds(120, 560, 117, 29);
 		frame.getContentPane().add(btnPre_image1);
 		
 		JButton btnNext_image1 = new JButton("next");
 		btnNext_image1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String firstImageUrl = numberImages.get((++firstImageIndex) % numberImages.size()).getUrls().get(IMAGE_LEFT);
-				label_image1.setIcon(getScaledImage(firstImageUrl, IMAGE_WIDTH, IMAGE_HEIGHT));
+				NumberImage numberImage = numberImages.get((++leftImageIndex) % numberImages.size());				
+				setImageAndNumber(numberImage.getUrls(), numberImage.getNumber(), leftImageIndex, true);
 			}
 		});
-		btnNext_image1.setBounds(290, 473, 117, 29);
+		btnNext_image1.setBounds(288, 560, 117, 29);
 		frame.getContentPane().add(btnNext_image1);
 		
 		JButton btnPre_image2 = new JButton("pre");
 		btnPre_image2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(secondImageIndex == 0) {
-					secondImageIndex += numberImages.size();
+				if(rightImageIndex == 0) {
+					rightImageIndex += numberImages.size();
 				}
-				String secondImageUrl = numberImages.get((--secondImageIndex) % numberImages.size()).getUrls().get(IMAGE_RIGHT);
-				label_image2.setIcon(getScaledImage(secondImageUrl, IMAGE_WIDTH, IMAGE_HEIGHT));
+				NumberImage numberImage = numberImages.get((--rightImageIndex) % numberImages.size()); 
+				setImageAndNumber(numberImage.getUrls(), numberImage.getNumber(), rightImageIndex, false);
 			}
 		});
-		btnPre_image2.setBounds(596, 473, 117, 29);
+		btnPre_image2.setBounds(596, 560, 117, 29);
 		frame.getContentPane().add(btnPre_image2);
 		
 		JButton btnNext_image2 = new JButton("next");
 		btnNext_image2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String secondImageUrl = numberImages.get((++secondImageIndex) % numberImages.size()).getUrls().get(IMAGE_RIGHT);
-				label_image2.setIcon(getScaledImage(secondImageUrl, IMAGE_WIDTH, IMAGE_HEIGHT));
+				NumberImage numberImage = numberImages.get((++rightImageIndex) % numberImages.size());					
+				setImageAndNumber(numberImage.getUrls(), numberImage.getNumber(), rightImageIndex, false);		
 			}
 		});
-		btnNext_image2.setBounds(762, 473, 117, 29);
+		btnNext_image2.setBounds(764, 560, 117, 29);
 		frame.getContentPane().add(btnNext_image2);
 		
-		String firstImageUrl = numberImages.get(0).getUrls().get(0);
+		NumberImage initialImage = numberImages.get(leftImageIndex);
 		
-		label_image1 = new JLabel(getScaledImage(firstImageUrl, IMAGE_WIDTH, IMAGE_HEIGHT));
-		label_image1.setBounds(40, 52, IMAGE_WIDTH, IMAGE_HEIGHT);			
+		label_image1 = new JLabel(getScaledImage(initialImage.getUrls().get(IMAGE_LEFT), IMAGE_WIDTH, IMAGE_HEIGHT));
+		label_image1.setBounds(40, 108, IMAGE_WIDTH, IMAGE_HEIGHT);			
 		frame.getContentPane().add(label_image1);
 		
-		label_image2 = new JLabel(getScaledImage(firstImageUrl, IMAGE_WIDTH, IMAGE_HEIGHT));
-		label_image2.setBounds(522, 52, IMAGE_WIDTH, IMAGE_HEIGHT);
+		label_image2 = new JLabel(getScaledImage(initialImage.getUrls().get(IMAGE_LEFT), IMAGE_WIDTH, IMAGE_HEIGHT));
+		label_image2.setBounds(522, 108, IMAGE_WIDTH, IMAGE_HEIGHT);
 		frame.getContentPane().add(label_image2);		
+		
+		label_leftNumber = new JLabel(initialImage.getNumber());
+		label_leftNumber.setBounds(40, 518, 61, 16);
+		frame.getContentPane().add(label_leftNumber);
+		
+		label_rightNumber = new JLabel(initialImage.getNumber());
+		label_rightNumber.setBounds(522, 518, 61, 16);
+		frame.getContentPane().add(label_rightNumber);
+		
+		textField_leftSearch = new JTextField();
+		textField_leftSearch.setBounds(40, 41, 130, 26);
+		frame.getContentPane().add(textField_leftSearch);
+		textField_leftSearch.setColumns(10);
+		
+		JButton btnLeftSearch = new JButton("left search");
+		btnLeftSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String searchedNumber = textField_leftSearch.getText().trim();
+				boolean findRes = false;
+				for(int i=0; i<numberImages.size(); i++) {
+					NumberImage numberImage = numberImages.get(i);
+					if(numberImage.getNumber().equals(searchedNumber)) {	
+						findRes = true;
+						setImageAndNumber(numberImage.getUrls(), numberImage.getNumber(), i, true);						
+					}
+				}
+				if(!findRes) {
+					JOptionPane.showMessageDialog(frame, "nothing find on left side!");
+				}
+			}
+		});
+		btnLeftSearch.setBounds(182, 41, 117, 29);
+		frame.getContentPane().add(btnLeftSearch);
+		
+		textField_rightSearch = new JTextField();
+		textField_rightSearch.setColumns(10);
+		textField_rightSearch.setBounds(522, 41, 130, 26);
+		frame.getContentPane().add(textField_rightSearch);
+		
+		btnRightSearch = new JButton("right_search");
+		btnRightSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String searchedNumber = textField_rightSearch.getText().trim();
+				boolean findRes = false;
+				for(int i=0; i<numberImages.size(); i++) {
+					NumberImage numberImage = numberImages.get(i);
+					if(numberImage.getNumber().equals(searchedNumber)) {						
+						findRes = true;
+						setImageAndNumber(numberImage.getUrls(), numberImage.getNumber(), i, false);					
+					}
+				}
+				if(!findRes) {
+					JOptionPane.showMessageDialog(frame, "nothing find on right side!");
+				}
+			}
+		});
+		btnRightSearch.setBounds(664, 41, 117, 29);
+		frame.getContentPane().add(btnRightSearch);
+		
+	}		
+	
+	private void setImageAndNumber(List<String> urls, String number, int imageIndex, boolean first) {
+		if(first) {
+			label_image1.setIcon(getScaledImage(urls.get(IMAGE_LEFT), IMAGE_WIDTH, IMAGE_HEIGHT));
+			label_leftNumber.setText(number);
+			leftImageIndex = imageIndex;
+		}else {
+			
+			label_image2.setIcon(getScaledImage(urls.size()==2 ? urls.get(IMAGE_RIGHT) : urls.get(IMAGE_LEFT), IMAGE_WIDTH, IMAGE_HEIGHT));
+			label_rightNumber.setText(number);
+			rightImageIndex = imageIndex;
+		}
 		
 	}
 	
 	private ImageIcon getScaledImage(String imagePath, int w, int h){				 
 		try {
-			Image srcImg = ImageIO.read(this.getClass().getResource(imagePath));
+			//Image srcImg = ImageIO.read(this.getClass().getResource(imagePath));
+			Image srcImg = ImageIO.read(new File(imagePath));
 			BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 			Graphics2D g2 = resizedImg.createGraphics();
 
@@ -160,7 +241,7 @@ public class ImageMainView {
 	}
 	
 	public List<NumberImage> readImagesFromDB() {
-		List<NumberImage> numberImages = new LinkedList<NumberImage>();		
+		numberImages = new LinkedList<NumberImage>();		
 		try {			
 			List<String> imageNumbers = dbManagement.getRecords();
 			if(!imageNumbers.isEmpty()) {
--- src/com/bcgkyy/test/ImageMainViewTest.java
+++ src/com/bcgkyy/test/ImageMainViewTest.java
@@ -1,6 +1,7 @@
 package com.bcgkyy.test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import com.bcgkyy.db.DBManagement;
 import com.bcgkyy.demo.NumberImage;
 import com.bcgkyy.images.ImageMainView;
 
@@ -27,15 +29,32 @@ public class ImageMainViewTest {
 	
 	@Test
 	public void readImageInfor() {
-		String imageFolderPath = "C:/Users/E068043/git/Memorize/img";
+		String imageFolderPath = "/Users/Alex/Downloads/images/";
+		DBManagement dbManagement = new DBManagement("imageNumber.xlsx", "imageMapping");
 		String outputFolderPath = "img/";				
 		File imageFolder = new File(imageFolderPath);
 		Map<String, List<String>> imageMaps = new HashMap<String, List<String>>();
 		for(File file : imageFolder.listFiles()) {
-			String fileName = file.getName().split(".")[0];
-			String imageUrl = outputFolderPath+"fileName.png";
-			List<String> urls = imageMaps.getOrDefault(fileName, new ArrayList<String>());
-			urls.add(imageUrl);
+			String fileName = file.getName();			
+			if(fileName.contains("png")) {
+				String number = file.getName().split("\\.")[0];				
+				if(number.contains("-")) {
+					number = number.split("-", 2)[0];
+				}
+				String imageUrl = outputFolderPath+fileName;
+				List<String> urls = imageMaps.getOrDefault(number, new ArrayList<String>());
+				urls.add(imageUrl);	
+				imageMaps.put(number, urls);
+			}			
+		}
+		for(String number : imageMaps.keySet()) {
+			String[] record = new String[] {number,String.join(",", imageMaps.get(number))};
+			try {
+				dbManagement.insertRecord(record);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
